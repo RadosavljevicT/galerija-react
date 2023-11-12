a14fd2f894207854501229c7503247fab5c78cd6
RadosavljevicT <radosavljevic.tamara97@gmail.com>
2023-11-12 23:01:22 +0100

    Napravljen projekat.

---
+++ galerije/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Search from './components/Search';
+
+function App() {
+  return (
+   <div className="App">
+    <Router>
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/search" element ={<Search />}/>
+      </Routes>
+  </Router>
+  </div>
+  );
+}
+
+export default App;
